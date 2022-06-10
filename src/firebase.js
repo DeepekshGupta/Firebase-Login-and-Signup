ebc013909b13de3b6e789e5d6fd08ee0f0ed3461
Deepeksh Gupta <76996419+DeepekshGupta@users.noreply.github.com>
2022-06-10 11:40:45 +0530

    chnage 1

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth"
 import 'firebase/firestore';
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyAuUuw0ktCw6a8kFcMb52VHlQlbI-h1DaA",
-  authDomain: "websiteauth-bb61e.firebaseapp.com",
-  projectId: "websiteauth-bb61e",
-  storageBucket: "websiteauth-bb61e.appspot.com",
-  messagingSenderId: "719470881378",
-  appId: "1:719470881378:web:56b088af35cf55da867f3b",
-  measurementId: "G-0RP8HSV2DM"
+  apiKey: "AIzaSyDLjhW3Ql_wUu6iZfZ1wFy5hDYvVjm0bLU",
+  authDomain: "instaclone-react-e504c.firebaseapp.com",
+  projectId: "instaclone-react-e504c",
+  storageBucket: "instaclone-react-e504c.appspot.com",
+  messagingSenderId: "256203631680",
+  appId: "1:256203631680:web:6fc1dbbaffc3a30b04c3a5",
+  measurementId: "G-KWTN4NCS08"
 })
 
 export const auth = app.auth()
